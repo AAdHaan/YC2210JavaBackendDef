a611d951e0afe6495769ae352a8e3273e92ed163
AAdHaan <aadehaan@hotmail.com>
2022-10-21 11:37:25 +0200

    eerste Initialisatie

---
+++ src/main/java/com/example/YC2210JavaBackendInit/Yc2210JavaBackendInitApplication.java
@@ -0,0 +1,13 @@
+package com.example.YC2210JavaBackendInit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yc2210JavaBackendInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yc2210JavaBackendInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YC2210JavaBackendInit/Yc2210JavaBackendInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YC2210JavaBackendInit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yc2210JavaBackendInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
